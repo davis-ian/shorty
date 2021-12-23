659ac9a7d20699c625a787827591399a579efa3a
ian <iandavis6481@gmail.com>
2021-12-23 12:20:47 -0800

    update

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn url_project.wsgi
+release: python manage.py migrate
--- url_project/settings.py
+++ url_project/settings.py
@@ -119,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_on_heroku
+django_on_heroku.settings(locals())
